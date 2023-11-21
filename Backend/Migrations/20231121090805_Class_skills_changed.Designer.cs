2ea709bc8afae201ba90df76923a5ed1bb3f150c
DetlefSauerbrei <sauerbrei@t-online.de>
2023-11-21 10:10:34 +0100

    Migration added

--- Backend/Migrations/20231121090805_Class_skills_changed.Designer.cs
+++ Backend/Migrations/20231121090805_Class_skills_changed.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace UGHApi.Migrations
 {
     [DbContext(typeof(UghContext))]
-    partial class UghContextModelSnapshot : ModelSnapshot
+    [Migration("20231121090805_Class_skills_changed")]
+    partial class Classskillschanged
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
