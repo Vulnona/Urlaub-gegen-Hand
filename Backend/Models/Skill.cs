a5258a6e26f54a44c5500235fa7fce0e0a848b60
DetlefSauerbrei <sauerbrei@t-online.de>
2023-11-21 10:09:40 +0100

    Removed Constructor

--- Backend/Models/Skill.cs
+++ Backend/Models/Skill.cs
@@ -4,16 +4,16 @@ namespace UGHApi.Models
 {
     public class Skill
     {
-        public Skill(int skill_ID, string skillDescrition, Skill parentSkill)
+        /* public Skill(int skill_ID, string skillDescrition,int? parentSkill_id)
         {
             this.Skill_ID = skill_ID;
             this.SkillDescrition = skillDescrition;
-            ParentSkill = parentSkill;
-        }
+            ParentSkill_ID = parentSkill_id;
+        } */
         [Key]
-        public int Skill_ID{get;}
+        public int Skill_ID{get;set;}
         [Required]
         public string SkillDescrition{get;set;}
-        public Skill ParentSkill{get;set;}
+        public Nullable<int>  ParentSkill_ID{get;set;}
     }
 }
