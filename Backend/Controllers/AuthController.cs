cb57abc727758a6cc6716dc056d20945a774af77
Vulnona <sturmiechen@googlemail.com>
2024-01-03 10:21:27 +0100

    Updated AuthController.cs

--- Backend/Controllers/AuthController.cs
+++ Backend/Controllers/AuthController.cs
@@ -1,5 +1,4 @@
 using System;
-<<<<<<< HEAD
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,13 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using UGHModels;
-=======
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using UGHModels;  
 using System.ComponentModel.DataAnnotations;
->>>>>>> merge-master-to-main
 
 namespace UGHApi.Controllers
 {
@@ -28,121 +21,6 @@ namespace UGHApi.Controllers
             _context = context;
         }
 
-<<<<<<< HEAD
-        // POST: api/Auth/Register
-        [HttpPost]
-        public async Task<ActionResult<User>> Register([FromBody] RegisterRequest request)
-        {
-            if (request.Email == null || request.Password == null || request.Username == null)
-            {
-                return BadRequest();
-            }
-
-            if (_context.Users.Any(e => e.Email == request.Email))
-            {
-                return Conflict();
-            }
-
-            var user = new User
-            {
-                Email = request.Email,
-                Password = request.Password,
-                Username = request.Username,
-            };
-
-            _context.Users.Add(user);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetUser", new { id = user.UserId }, user);
-        }
-
-        // POST: api/Auth/Login
-        [HttpPost]
-        public async Task<ActionResult<User>> Login([FromBody] LoginRequest request)
-        {
-            if (request.Email == null || request.Password == null)
-            {
-                return BadRequest();
-            }
-
-            var user = await _context.Users.FirstOrDefaultAsync(e => e.Email == request.Email && e.Password == request.Password);
-
-            if (user == null)
-            {
-                return Unauthorized();
-            }
-
-            return user;
-        }
-
-        // POST: api/Auth/VerifyEmail
-        [HttpPost]
-        public async Task<ActionResult<string>> VerifyEmail([FromBody] VerifyEmailRequest request)
-        {
-            var user = await _context.Users.FirstOrDefaultAsync(e => e.Email == request.Email);
-
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            user.EmailVerified = true;
-            await _context.SaveChangesAsync();
-
-            return "Die E-Mail-Adresse wurde verifiziert.";
-        }
-
-        // POST: api/Auth/ResendVerificationEmail
-        [HttpPost]
-        public async Task<ActionResult<string>> ResendVerificationEmail([FromBody] ResendVerificationEmailRequest request)
-        {
-            var user = await _context.Users.FirstOrDefaultAsync(e => e.Email == request.Email);
-
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            var link = "https://example.com/verify?id=" + user.UserId;
-
-            var email = new EmailMessage()
-            {
-                Subject = "E-Mail-Adresse verifizieren",
-                Body = $"Um Ihre E-Mail-Adresse zu verifizieren, klicken Sie bitte auf den folgenden Link: {link}",
-                To = user.Email,
-            };
-
-            await _context.Emails.AddAsync(email);
-            await _context.SaveChangesAsync();
-
-            return "Die E-Mail wurde erneut versendet.";
-        }
-
-        public class RegisterRequest
-        {
-            public string Email { get; set; }
-            public string Password { get; set; }
-            public string Username { get; set; }
-        }
-
-        public class LoginRequest
-        {
-            public string Email { get; set; }
-            public string Password { get; set; }
-        }
-
-        public class VerifyEmailRequest
-        {
-            public string Email { get; set; }
-        }
-
-        public class ResendVerificationEmailRequest
-        {
-            public string Email { get; set; }
-        }
-    }
-}
-=======
         [HttpPost("register")]
         public async Task<ActionResult<User>> Register([FromBody] RegisterRequest request)
         {
@@ -216,4 +94,4 @@ namespace UGHApi.Controllers
         public bool IsEmailVerified { get; set; }
     }
 }
->>>>>>> merge-master-to-main
+
