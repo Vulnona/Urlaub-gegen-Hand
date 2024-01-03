445484ec3ade796d659a0907c12ef3d29d049a72
Vulnona <sturmiechen@googlemail.com>
2024-01-03 10:48:11 +0100

    added ughapi.services

--- Backend/Controllers/AuthController.cs
+++ Backend/Controllers/AuthController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using UGHModels;
+using UGHApi.Services;
 using System.ComponentModel.DataAnnotations;
 
 namespace UGHApi.Controllers
