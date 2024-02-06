319308c551e5933f06a8a528e4a1e85af074c52a
Katharina Tan <k.tan@neusta.de>
2024-02-06 01:06:17 +0100

    UGH-43: (Account) login, detail, edit
    * get all personal Data from fake api and add it to accountDetail
    * add modal for accountModes
    * add modal for accountEdit

--- Frontend-Vuetify/src/components/HeaderMetaNav.vue
+++ Frontend-Vuetify/src/components/HeaderMetaNav.vue
@@ -4,43 +4,47 @@
       <li>
         <RouterLink to="account">
           <v-icon icon="mdi-account-circle-outline" />
-          <span class="label">
-            Benutzerkonto
-            </span>
+          <span class="label"> Benutzerkonto </span>
         </RouterLink>
       </li>
       <li>
         <RouterLink to="offer">
           <v-icon icon="mdi-map-plus" />
-          <span class="label">
-            Angebot erstellen
-            </span>
+          <span class="label"> Angebot erstellen </span>
         </RouterLink>
       </li>
       <li>
-        <button @click.prevent="isLoggedIn=false">
+        <button @click.prevent="doLogout">
           <v-icon icon="mdi-logout-variant" />
-          <span class="label">
-            Abmelden
-            </span>
+          <span class="label"> Abmelden </span>
         </button>
       </li>
     </template>
     <li v-else>
-      <button @click.prevent="isLoggedIn=true">
+      <button @click.prevent="doLogin">
         <v-icon icon="mdi-login-variant" />
-        <span class="label">
-            Anmelden
-            </span>
+        <span class="label"> Anmelden </span>
       </button>
     </li>
   </ul>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { computed } from "vue";
+import { useAppStore } from "@/store/app";
+import { useRouter } from "vue-router";
 
-const isLoggedIn = ref(true)
+const { push } = useRouter();
+const store = useAppStore();
+const isLoggedIn = computed(() => store.isLoggedIn);
+
+function doLogout() {
+  store.user = null;
+}
+
+function doLogin() {
+  push({ name: "Account" });
+}
 </script>
 
 <style lang="scss" scoped>
--- Frontend-Vuetify/src/views/Account.vue
+++ Frontend-Vuetify/src/views/Account.vue
@@ -1,76 +1,51 @@
 <template>
-  Account
+  <AccountDetail v-if="isLoggedIn" />
+  <v-row
+    v-else
+    class="justify-center"
+  >
+    <v-card
+      width="500"
+    >
+      <v-tabs
+        v-model="accountMode"
+        color="primary"
+        align-tabs="center"
+      >
+        <v-tab :value="AccountModes.login">
+          Anmelden
+        </v-tab>
+        <v-tab :value="AccountModes.register">
+          Registrieren
+        </v-tab>
+      </v-tabs>
+
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <v-col cols="12">
+              <AccountLogin v-if="accountMode === AccountModes.login" />
+              <AccountRegister v-else-if="accountMode === AccountModes.register" />
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
+    </v-card>
+  </v-row>
 </template>
 
 <script lang="ts" setup>
-/*
-https://dummyjson.com/users
-    {
-      "id": 1,
-      "firstName": "Terry",
-      "lastName": "Medhurst",
-      "maidenName": "Smitham",
-      "age": 50,
-      "gender": "male",
-      "email": "atuny0@sohu.com",
-      "phone": "+63 791 675 8914",
-      "username": "atuny0",
-      "password": "9uQFF1Lh",
-      "birthDate": "2000-12-25",
-      "image": "https://robohash.org/Terry.png?set=set4",
-      "bloodGroup": "A-",
-      "height": 189,
-      "weight": 75.4,
-      "eyeColor": "Green",
-      "hair": {
-        "color": "Black",
-        "type": "Strands"
-      },
-      "domain": "slashdot.org",
-      "ip": "117.29.86.254",
-      "address": {
-        "address": "1745 T Street Southeast",
-        "city": "Washington",
-        "coordinates": {
-          "lat": 38.867033,
-          "lng": -76.979235
-        },
-        "postalCode": "20020",
-        "state": "DC"
-      },
-      "macAddress": "13:69:BA:56:A3:74",
-      "university": "Capitol University",
-      "bank": {
-        "cardExpire": "06/22",
-        "cardNumber": "50380955204220685",
-        "cardType": "maestro",
-        "currency": "Peso",
-        "iban": "NO17 0695 2754 967"
-      },
-      "company": {
-        "address": {
-          "address": "629 Debbie Drive",
-          "city": "Nashville",
-          "coordinates": {
-            "lat": 36.208114,
-            "lng": -86.58621199999999
-          },
-          "postalCode": "37076",
-          "state": "TN"
-        },
-        "department": "Marketing",
-        "name": "Blanda-O'Keefe",
-        "title": "Help Desk Operator"
-      },
-      "ein": "20-9487066",
-      "ssn": "661-64-2976",
-      "userAgent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/12.0.702.0 Safari/534.24",
-      "crypto": {
-        "coin": "Bitcoin",
-        "wallet": "0xb9fc2fe63b2a6c003f1c324c3bfa53259162181a",
-        "network": "Ethereum (ERC20)"
-      }
+import { computed, ref} from 'vue';
+import { useAppStore} from '@/store/app';
+import AccountLogin from '@/components/account/AccountLogin.vue'
+import AccountRegister from '@/components/account/AccountRegister.vue'
+import AccountDetail from '@/components/account/AccountDetail.vue';
 
-*/
-
+enum AccountModes {
+  login,
+  register
+}
+const store = useAppStore()
+const isLoggedIn = computed(() => store.isLoggedIn)
+const accountMode = ref(AccountModes.login)
 </script>
