38a5aa2aa7fb003c873680da1cca3ac4b394cbac
dotnetexpert <expert.dnet@gmail.com>
2024-08-08 15:12:45 +0530

    Update vite.config.ts

--- Frontend-Vuetify/vite.config.ts
+++ Frontend-Vuetify/vite.config.ts
@@ -42,9 +42,7 @@ export default defineConfig(({ mode }) => {
     ],
     define: {
       'process.env': {
-        'SECRET_KEY': 'thisismytestsecretkey', 'baseURL': 'http://localhost:8080/api/', 'baseURL_Frontend': 'http://localhost:3000/',
-        'SecretAccessKey': 'G5BssUS++VqZ7WtQ14ivN9i6LDn/s+GJVwDdI9bH', 'AccessKeyId': 'AKIA5FTZAYJ5ASDPKHL6', 'Aws_region': 'eu-north-1',
-        'S3_BUCKET_NAME': 'urlaub-gegen-hand','x-api-key': '79ea7130-21a6-11ef-9447-bb18dd052b84',
+        'SECRET_KEY': 'thisismytestsecretkey', 'baseURL': 'http://localhost:8080/api/', 'baseURL_Frontend': 'http://localhost:3000/'
       }
     },
     resolve: {
