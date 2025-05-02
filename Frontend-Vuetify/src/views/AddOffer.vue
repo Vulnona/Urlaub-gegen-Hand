40bb02a0f79aeab0802b2b922f9efb06716473f2
user <l.bohlin@gmx.de>
2025-05-02 17:10:15 +0200

    refactored add-offer

--- Frontend-Vuetify/src/views/AddOffer.vue
+++ Frontend-Vuetify/src/views/AddOffer.vue
@@ -1,5 +1,9 @@
 <template>
 <Navbar />
+<div v-if="loading"  class="spinner-container text-center">
+  <div class="spinner"></div>
+</div>
+<div v-else>
 <div class="create-offer-wrapper bg-ltgrey">
   <Banner n="Erstelle Angebot" />
   <div class="bg-ltgrey pt-40 pb-40">
@@ -27,13 +31,13 @@
                     
                     <div class="row">
                       <div class="col">
-                        <label for="FromDate">Möglich ab<b style="color: red;">*</b></label>
-                 <Datepicker date v-model="FromDate" :locale="de" :enable-time-picker="false"
+                        <label for="offer.FromDate">Möglich ab<b style="color: red;">*</b></label>
+                 <Datepicker date v-model="offer.FromDate" :locale="de" :enable-time-picker="false"
                              :auto-apply="true" placeholder="dd.mm.yyyy" :typeable="true" input-class-name="datepicker-input" startingView='month'  inputFormat="dd.MM.yyyy"/>
                       </div>
                       <div class="col">
-                        <label for="UntilDate">Möglich bis<b style="color: red;">*</b></label>
-                        <Datepicker date v-model="UntilDate" :locale="de" :enable-time-picker="false"
+                        <label for="offer.UntilDate">Möglich bis<b style="color: red;">*</b></label>
+                        <Datepicker date v-model="offer.UntilDate" :locale="de" :enable-time-picker="false"
                                     :auto-apply="true" placeholder="dd.mm.yyyy" :typeable="true" input-class-name="datepicker-input" startingView='month'  inputFormat="dd.MM.yyyy"/>
                       </div>
                       </div>
@@ -54,8 +58,8 @@
                                  label="skillDescrition" track-by="skill_ID" multiple></multiselect>
                   </div>
                   <div class="amenities-wrapper">
-                    <div class="form-group"> <label> Unterbringung </label> <div class="contact_infoBox">
-                        <ul>
+                    <div class="form-group"> <label> Unterbringung </label> <div class="contact_infoBox">                        
+                        <ul>                          
                           <li v-for="accommodation in accommodations" :key="accommodation.id">
                             <div class="flexBox gap-x-2 image-withCheckbox">
                               <label class="checkbox_container">{{ accommodation.nameAccommodationType }}
@@ -102,20 +106,17 @@
         </div>
       </form>
     </div>
-  </div>
+</div>
+</div>
 </template>
 
 <script setup lang="ts">
-import { ref, defineExpose, onMounted, onUpdated } from "vue";
+import { ref, defineExpose, onMounted, onUpdated, reactive } from "vue";
 import Banner from '@/components/Banner.vue';
 import Input from '@/components/form/Input.vue';
 import Navbar from '@/components/navbar/Navbar.vue';
 import { de } from 'date-fns/locale';
 import Datepicker from 'vue3-datepicker';
-
-</script>
-
-<script lang="ts">
 import router from '@/router';
 import Multiselect from 'vue-multiselect';
 import 'vue-multiselect/dist/vue-multiselect.css';
@@ -123,89 +124,48 @@ import Securitybot from '@/services/SecurityBot';
 import axiosInstance from '@/interceptor/interceptor';
 import toast from '@/components/toaster/toast';
 
-export default {
-  components: {
-    Multiselect,
-  },
-  data() {
-    return {
-      toast: null,
-      offer: {
-          title: '',
-          description: '',
-          location: '',
-          accommodation: [],
-          accommodationSuitable: [],
-          skills: [],
-          image: null,          
-      },
-        skills: [],
-        accommodations: [],
-        suitableAccommodations: [],
-        UntilDate: null,
-        FromDate: new Date(),
-    };
-  },
 
-  mounted() {
-    Securitybot();
-    this.fetchSkills();
-    this.fetchAccommodations();
-      this.fetchSuitableAccommodations();
-  },
-  methods: {
-    // Method to fetch accommodations data
-    async fetchAccommodations() {
-      try {
-        const response = await axiosInstance.get(`${process.env.baseURL}accommodation/get-all-accommodations`);
-        this.accommodations = response.data;
-      } catch (error) {
-        // console.error('Error fetching accommodations:', error);
-      }
-    },
-    // Method to fetch suitable accommodations data
-    async fetchSuitableAccommodations() {
-      try {
-        const response = await axiosInstance.get(`${process.env.baseURL}accommodation-suitability/get-all-suitable-accommodations`);
-        this.suitableAccommodations = response.data;
-      } catch (error) {
-        //  console.error('Error fetching suitable accommodations:', error);
-      }
-    },
-    // Method to fetch skills data
-    async fetchSkills() {
-      try {
-        const response = await axiosInstance.get(`${process.env.baseURL}skills/get-all-skills`);
-        this.skills = response.data;
-      } catch (error) {
-        // console.error('Error fetching skills:', error);
-      }
-    },
-    async createOffer() {
-        // Method to create a new offer
-      if (this.offer.image && this.offer.image.size > 3.5 * 1024 * 1024) {
+let loading = ref(true);
+let offer = reactive ({
+    title: '',
+    description: '',
+    location: '',
+    accommodation: [],
+    accommodationSuitable: [],
+    skills: [],
+    image: '',
+    UntilDate: null,
+    FromDate: null
+});
+let skills = [];
+let accommodations = [];
+let suitableAccommodations = [];
+
+const createOffer = async() => {
+        loading = true;
+      if (offer.image && offer.image.size > 3.5 * 1024 * 1024) {
         toast.warning("Image size cannot be greater than 3.5 MB.");
         return;
       }
-      if (!this.offer.title || !this.offer.skills.length || !this.offer.image || !this.offer.description || !this.offer.location) {
+      if (!offer.title || !offer.skills.length || !offer.image || !offer.description || !offer.location) {
         toast.info("Please fill all the required fields marked with *");
         return;
       }
         const offerData = new FormData();
-        offerData.append('title', this.offer.title);
-        offerData.append('description', this.offer.description);
-        offerData.append('location', this.offer.location);
-        offerData.append('contact', this.offer.contact);
-        offerData.append('accommodation', this.offer.accommodation.join(', '));
-        offerData.append('accommodationSuitable', this.offer.accommodationSuitable.join(', '));
-        offerData.append('ToDate', this.UntilDate.toISOString().split('+')[0]);
-        offerData.append('FromDate', this.FromDate.toISOString().split('+')[0]);
-        offerData.append('skills', this.offer.skills.map(skill => skill.skillDescrition).join(', '));
+        offerData.append('title', offer.title);
+        offerData.append('description', offer.description);
+        offerData.append('location', offer.location);
+        offerData.append('contact', offer.contact);
+        offerData.append('accommodation', offer.accommodation.join(', '));
+        offerData.append('accommodationSuitable', offer.accommodationSuitable.join(', '));
+        offerData.append('ToDate', offer.UntilDate.toISOString().split('+')[0]);
+        offerData.append('FromDate', offer.FromDate.toISOString().split('+')[0]);
+        offerData.append('skills', offer.skills.map(skill => skill.skillDescrition).join(', '));
         offerData.append('country', "");
         offerData.append('state', "");
         offerData.append('city', "");        
-      if (this.offer.image) {
-        offerData.append('image', this.offer.image);
+      if (offer.image) {
+        offerData.append('image', offer.image);
       }
       try {
         const response = await axiosInstance.post(
@@ -222,13 +182,22 @@ export default {
       } catch (error) {
         toast.info("Unable To Create Offer!");
       }
-    },
-    onFileChange(event) {
-      this.offer.image = event.target.files[0];
+       loading = false;
+}
+const onFileChange = (event) => {
+      offer.image = event.target.files[0];
     }
-  }
-};
+
+onMounted(async () => {
+    loading.value = true;
+    accommodations = (await axiosInstance.get(`${process.env.baseURL}accommodation/get-all-accommodations`)).data;
+    suitableAccommodations = (await axiosInstance.get(`${process.env.baseURL}accommodation-suitability/get-all-suitable-accommodations`)).data;
+    skills = (await axiosInstance.get(`${process.env.baseURL}skills/get-all-skills`)).data;
+    loading.value = false;
+})
 </script>
+
+
 <style scoped>
 .desc-textarea {height: 90px;}
 </style>
